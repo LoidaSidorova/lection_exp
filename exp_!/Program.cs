72d03d15168a5488fb4a0af2bc6ab025136c6200
oida <kuksoloida@mail.ru>
2023-09-19 11:24:07 +0300

    add summ 2 numberous

--- exp_!/Program.cs
+++ exp_!/Program.cs
@@ -8,4 +8,17 @@
 //Console.Write(username);
 //Console.Write("!");
 
-//
+//написать программу, выводящую сумму двух натуральных чисел на экран.
+
+//Console.WriteLine("Введите два числа ");
+//int numberA=Console.ReadLine();
+//int numberB=Console.ReadLine();
+//Console.WriteLine(numberA+numberB);
+
+//написать программу выводящую частное двух чисел.
+int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+int result = numberA+numberB;
+Console.WriteLine(result);
