1af79f28bf6568bcb28a9010e4551073dea8fca1
oida <kuksoloida@mail.ru>
2023-09-19 12:49:10 +0300

    add exp_006

--- exp_!/Program.cs
+++ exp_!/Program.cs
@@ -20,24 +20,42 @@
 
 //Example_004
 //написать программу выводящую частное двух чисел.
-//int numberA = new Random().Next(1,10);
-//int numberB = new Random().Next(1,10);
+//double numberA = new Random().Next(1,10);
+//double numberB = new Random().Next(1,10);
 //Console.WriteLine(numberA);
 //Console.WriteLine(numberB);
-//int result = numberA+numberB;
+//double result = numberA/numberB;
 //Console.WriteLine(result);
 
 //Example_005
 //Написать программу, приветствующую "любимчика" по-особенному.
-Console.WriteLine("Введите ваше имя: ");
-string username = Console.ReadLine();
+//Console.WriteLine("Введите ваше имя: ");
+//string username = Console.ReadLine();
 
-if(username.ToLower() == "Маша")
-{
-    Console.WriteLine("Ура, это же Маша!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+//if(username.ToLower() == "Маша")
+//{
+//Console.WriteLine("Ура, это же Маша!");
+//}
+//else
+//{
+//  Console.Write("Привет, ");
+//Console.WriteLine(username);
+//}
+
+//Example_006
+
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
