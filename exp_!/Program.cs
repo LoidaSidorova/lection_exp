0f8ab3f714956f383b3b4d6c2f0f6a86f1e79f0a
oida <kuksoloida@mail.ru>
2023-09-19 10:51:41 +0300

    add 2nd exp from lection

---
+++ exp_!/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача вывода Hello World!
+// Console.WriteLine("123 Hello, World!");
+
+// Задача вывода приветствия пользователя.
+//Console.WriteLine("Введите Ваше имя ");
+//string username=Console.ReadLine();
+//Console.Write("Привет, ");
+//Console.Write(username);
+//Console.Write("!");
+
+//
