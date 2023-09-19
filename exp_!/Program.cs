2df289ae73531053423f40ea869ceae5c55d30f1
oida <kuksoloida@mail.ru>
2023-09-19 12:23:46 +0300

    add exp_5

--- exp_!/Program.cs
+++ exp_!/Program.cs
@@ -1,6 +1,8 @@
-﻿// Задача вывода Hello World!
+﻿//Example_001
+// Задача вывода Hello World!
 // Console.WriteLine("123 Hello, World!");
 
+//Example_002
 // Задача вывода приветствия пользователя.
 //Console.WriteLine("Введите Ваше имя ");
 //string username=Console.ReadLine();
@@ -8,17 +10,34 @@
 //Console.Write(username);
 //Console.Write("!");
 
+//Example_003
 //написать программу, выводящую сумму двух натуральных чисел на экран.
 
 //Console.WriteLine("Введите два числа ");
-//int numberA=Console.ReadLine();
-//int numberB=Console.ReadLine();
+//int numberA=Convert.ToInt32(Console.ReadLine());
+//int numberB=Convert.ToInt32(Console.ReadLine());
 //Console.WriteLine(numberA+numberB);
 
+//Example_004
 //написать программу выводящую частное двух чисел.
-int numberA = new Random().Next(1,10);
-int numberB = new Random().Next(1,10);
-Console.WriteLine(numberA);
-Console.WriteLine(numberB);
-int result = numberA+numberB;
-Console.WriteLine(result);
+//int numberA = new Random().Next(1,10);
+//int numberB = new Random().Next(1,10);
+//Console.WriteLine(numberA);
+//Console.WriteLine(numberB);
+//int result = numberA+numberB;
+//Console.WriteLine(result);
+
+//Example_005
+//Написать программу, приветствующую "любимчика" по-особенному.
+Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
